a7619c27860e206beb9b9662f754fc0d04fa96ca
Aphi66 <150765717+Aphi66@users.noreply.github.com>
2025-08-02 13:16:17 +0700

    BE1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql2/promise');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,28 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('register'); // render register.ejs
 });
+
+// Database connection
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'non1150',
+  database: 'Upam',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+async function testConnection() {
+  try {
+    const [rows] = await pool.query('SELECT 1 + 1 AS result');
+    console.log('Connection Result:', rows[0].result);
+  } catch (err) {
+    console.error('Connection failed:', err);
+  }
+}
+
+testConnection();
 
 // Start server
 app.listen(PORT, () => {
